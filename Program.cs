b9947c9c525c8dd8ca1e7166a7f6d4120e3a9f0d
Aliice77 <69522330+Aliice77@users.noreply.github.com>
2022-05-23 11:23:33 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace session1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            string b="       ";
+            string c="";
+            string d="";
+            for (int i=1;i<6;i++)
+            {
+                c+=$"{i}";
+                Console.WriteLine(b.Substring(i)+c+d);
+                d=$"{i}{d}";
+            }
+          
+        }
+    }
+    
+}
